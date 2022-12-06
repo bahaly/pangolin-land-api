d1755dbd25e028e4da50a5429af78f8214dff7bd
bahaly <50265605+bahaly@users.noreply.github.com>
2022-12-06 19:02:47 +0000

    return user data on signup

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,7 @@ exports.signup = (req, res, next) => {
           imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         });
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !', data: user }))
           .catch(error => {console.log(error); return res.status(400).json({ error })});
       })
       .catch(error => {console.log(error);return res.status(500).json({ error })});
